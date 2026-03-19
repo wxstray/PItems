d470984d00c534af8fdb6aa619d9c2cdb29d6d2f
Victualler <victuallerdev@gmail.com>
2026-03-18 19:21:20 -0600

    feat: add hide hearts item

---
+++ src/voidworks/ppitems/items/preset/HideHearts.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace voidworks\ppitems\items\preset;
+
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\item\VanillaItems;
+use pocketmine\network\mcpe\protocol\SetHudPacket;
+use pocketmine\network\mcpe\protocol\types\hud\HudElement;
+use pocketmine\network\mcpe\protocol\types\hud\HudVisibility;
+use pocketmine\player\Player;
+use pocketmine\utils\TextFormat;
+use ReflectionException;
+use voidworks\ppitems\items\BasePartnerItem;
+use voidworks\ppitems\items\impl\OnAttackPartnerItem;
+use voidworks\ppitems\items\impl\OnUsePartnerItem;
+use voidworks\ppitems\Loader;
+use voidworks\ppitems\utils\TimedListener;
+
+class HideHearts extends BasePartnerItem implements OnAttackPartnerItem {
+
+    public function __construct() {
+        parent::__construct(
+            'hidehearts',
+            TextFormat::colorize('&r&4Hide Hearts'),
+            VanillaItems::HEART_OF_THE_SEA()
+        );
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function onAttack(Player $damager, Player $player): void {
+        $packet = SetHudPacket::create([HudElement::HEALTH], HudVisibility::HIDE);
+        $player->getNetworkSession()->sendDataPacket($packet);
+
+        $player->sendMessage(TextFormat::colorize('&r&7Your health is now hidden for &f5 &7seconds.'));
+        $damager->sendMessage(TextFormat::colorize('&r&7You have hidden the health of &f' . $player->getName() . '&7.'));
+
+        TimedListener::listen(
+            Loader::getInstance(),
+            function () use ($player): void {
+                $resetPacket = SetHudPacket::create([HudElement::HEALTH], HudVisibility::RESET);
+                $player->getNetworkSession()->sendDataPacket($resetPacket);
+            },
+            delay: 20 * 5
+        );
+    }
+}
